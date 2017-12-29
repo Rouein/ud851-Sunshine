82c212e4c5149b79d38178db4511750637a2c805
rouein <rouein.fani@gmail.com>
2017-12-29 21:42:21 +0300

    Done

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,6 +16,11 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
+
+import com.example.android.sunshine.R;
 
 public class SunshinePreferences {
 
@@ -87,9 +92,10 @@ public class SunshinePreferences {
      * "94043,USA" if SharedPreferences have not been implemented yet.
      */
     public static String getPreferredWeatherLocation(Context context) {
-        // TODO (1) Return the user's preferred location
+        // Done (1) Return the user's preferred location
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         /** This will be implemented in a future lesson **/
-        return getDefaultWeatherLocation();
+        return preferences.getString(context.getString(R.string.pref_location_key), context.getString(R.string.pref_location_default));
     }
 
     /**
@@ -100,9 +106,16 @@ public class SunshinePreferences {
      * @return true If metric display should be used
      */
     public static boolean isMetric(Context context) {
-        // TODO (2) Return true if the user's preference for units is metric, false otherwise
+        // Done (2) Return true if the user's preference for units is metric, false otherwise
         /** This will be implemented in a future lesson **/
-        return true;
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String preferredUnits = preferences.getString(context.getString(R.string.pref_units_key), context.getString(R.string.pref_units_metric));
+
+        if (context.getString(R.string.pref_units_metric).equals(preferredUnits)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
